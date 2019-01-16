54cb288bb5604675c4c6eaf67fc9e1e406392585
oceangypsy <oleksandra.altukhova@gmail.com>
2019-01-16 15:09:07 +0200

    Initial Commit

---
+++ RSS-Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSS-Reader
+//
+//  Created by  Oleksandra on 1/16/19.
+//  Copyright © 2019 sandra-alt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
